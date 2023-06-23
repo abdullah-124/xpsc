78c9a10e39326e1e44d2713374e41e29125c338a
abdullah-124 <mdabdullahsakib468@gmail.com>
2023-06-23 10:43:37 +0600

    week -3

---
+++ week-3/day-7/A_Compare_T_Shirt_Sizes.cpp
@@ -0,0 +1,31 @@
+#include<bits/stdc++.h>
+#define fastread() {ios_base::sync_with_stdio(false);cin.tie(0);cout.tie(0);}
+#define ll long long
+using namespace std;
+void solve(){
+    string a,b; cin>>a>>b;
+    if(a==b){
+        cout<<"="<<endl;
+        return;
+    }
+    char ca = a.back();
+    char cb = b.back();
+    if(ca == cb && ca == 'S'){
+        if(a.size() > b.size())cout<<"<" <<endl;
+        else cout<<">"<<endl;
+    }
+    else if(ca == cb){
+        if(a.size()>b.size())cout<<">"<<endl;
+    }
+    else {
+        if(ca>cb)cout<<"<"<<endl;
+        else cout<<">"<<endl;
+    }
+    
+}
+int main(){
+    fastread()
+    int t;  cin>>t;
+    while(t--)solve();
+    return 0;
+}
