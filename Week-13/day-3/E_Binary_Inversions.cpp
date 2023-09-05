12ea8ca61493660f75edd989078015be6e842e9e
abdullah-124 <mdabdullahsakib468@gmail.com>
2023-09-05 23:09:19 +0600

    jjj

--- Week-13/day-3/E_Binary_Inversions.cpp
+++ Week-13/day-3/E_Binary_Inversions.cpp
@@ -7,10 +7,26 @@ void solve(){
     int n;    cin>>n;
     int oc=0;
     int ans = 0;
+    int a[n];
+    int id1=1,id0 = -1;
     for(int i=0;i<n;i++){
-        int x;  cin>>x;
-        if(x==1)oc++;
-        if(x==0)ans+=oc;
+        cin>>a[i];
+        if(a[i]==1)id1 = i;
+        else if(id0==-1)id0 = i;
+    }
+    if(id0==-1){
+        id0 = id1;
+        a[id0] =0;
+    }else{
+        a[id0] = 1;
+    }
+    
+    for(int i=0;i<n;i++){
+        cout<<a[i]<<' ';
+        if(a[i]==1)oc++;
+        else{
+            ans += oc;
+        }
     }
     cout<<ans<<endl;
 }
